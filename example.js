fd073a00bc7db7df950a447e3b2a2fa9f68e82cf
romina1127 <diaz-riveror@stlcaps.net>
2020-11-30 11:04:56 -0600

    added some comments and new variable

--- example.js
+++ example.js
@@ -1,18 +1,19 @@
 var name = "Romina";
 var ex = 4*60;
-const constExample = 500;
+const constExample = 500;  // const variable example
 var exVariable = ex + constExample;
 var exAssignment = 5;
 exAssignment += 5;
 
-var carPrice = 2000;
+var carPrice = 2000;  
 var tirePrice = 500;
 var detailPrice = 700;
+var totalPackagePrice = carPrice + tirePrice + detailPrice - 100;
 
-var isGrassGreen = true;
+var isGrassGreen = true;  // Boolean example
 
 {
-    let name = "Steve"
+    let name = "Steve"  // Example for how let works compared to var
 }
 
 function letExample() {
