1f45b4d3ba97d6e8d5768a2d6983df88face1b32
romina1127 <diaz-riveror@stlcaps.net>
2020-11-29 18:34:39 -0600

    added examples files for class to do

---
+++ example.js
@@ -0,0 +1,23 @@
+var name = "Romina";
+var ex = 4*60;
+const constExample = 500;
+var exVariable = ex + constExample;
+var exAssignment = 5;
+exAssignment += 5;
+
+var carPrice = 2000;
+var tirePrice = 500;
+var detailPrice = 700;
+var wordVar = rlkajdslks; 
+
+{
+    let name = "Steve"
+}
+
+function letExample() {
+    let name = "Steve";
+    return name;
+}
+
+document.getElementById("demo").innerHTML = name + " is my name.";
+document.getElementById("demo-second").innerHTML = constExample + " is a constant variable";
