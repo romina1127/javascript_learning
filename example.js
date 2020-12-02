4a7bc31e355b2b661aee2689921b3c0061d11ed6
romina1127 <diaz-riveror@stlcaps.net>
2020-12-02 12:03:58 -0600

    added comments and console logs

--- example.js
+++ example.js
@@ -1,14 +1,26 @@
+// var price = 5;
+// let price = 7;
 var name = "Romina";
 var ex = 4*60;
 const constExample = 500;  // const variable example
 var exVariable = ex + constExample;
 var exAssignment = 5;
-exAssignment += 5;
+exAssignment += 5;  // += operator example on how to change/manipulate variables
+
+let goodExample = 50;  // Examples on how to label variables properly.  you can start with letters
+let $dollarExample = 40;
+let _underscoreExample = 2;
+let number5757example = 100;
 
 var carPrice = 2000;  
 var tirePrice = 500;
 var detailPrice = 700;
 var totalPackagePrice = carPrice + tirePrice + detailPrice - 100;
+
+var array = [5, 6, 7, 8, "Romina", true];  // array example
+console.log(array[4] + " array example")
+
+var object = {prop: 4, prop2: 5, prop: 76};
 
 var isGrassGreen = true;  // Boolean example
 
@@ -17,9 +29,15 @@ var isGrassGreen = true;  // Boolean example
 }
 
 function letExample() {
-    var name = "Steve";
+    let name = "Steve";
     return name;
 }
+
+console.log(goodExample*$dollarExample)
+console.log(number5757example+$dollarExample)
+console.log(goodExample/_underscoreExample)
+console.log(goodExample%$dollarExample)
+console.log(number5757example^_underscoreExample)
 
 // document.getElementById("demo").innerHTML = name + " is my name.";
 // document.getElementById("demo-second").innerHTML = constExample + " is a constant variable";
